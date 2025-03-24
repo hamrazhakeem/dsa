36f5becb8c86228bd1a1c3865d8560f3ac6be9c2
Hamraz Hakeem <147601279+hamrazhakeem@users.noreply.github.com>
2025-03-24 09:45:46 +0530

    feat: Remove redundant conditional check in selection sort swap operation

--- DS2/Algorithms/selection_sort.py
+++ DS2/Algorithms/selection_sort.py
@@ -4,8 +4,7 @@ def selection_sort(arr):
         for j in range(i+1, len(arr)):
             if arr[j] < arr[min_idx]:
                 min_idx = j
-        if i != min_idx:
-            arr[min_idx], arr[i] = arr[i], arr[min_idx]
+        arr[min_idx], arr[i] = arr[i], arr[min_idx]
 
 arr = [7, 3, 8, 5, 9]
 selection_sort(arr)
